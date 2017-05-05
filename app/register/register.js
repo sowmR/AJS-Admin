2c3492453fae713341db7c82bef1c767ee4565c6
sowm <sowmyas@gmail.com>
2017-05-05 11:21:40 -0400

    files updated

---
+++ app/register/register.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.register', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/register', {
+    templateUrl: 'register/register.html',
+    controller: 'registerCtrl'
+  });
+}])
+
+.controller('registerCtrl', [function() {
+
+}]);
